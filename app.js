d7e52496d7c9f181f11aba2da2f4675a98efaf45
sinjouji <mail0of0iris@gmail.com>
2026-06-20 10:48:20 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -68,11 +68,11 @@ let db = null;
    Firebase Console → プロジェクトの設定 → マイアプリ → SDKの設定と構成
 ══════════════════════════════════════ */
 const FIREBASE_CONFIG = {
-  apiKey : AIzaSyBbKlHBjsu9-qKpWVBBm4rFZinfJOek5Hc,
-authDomain : mychls-p0intapp.firebaseapp.com,
-databaseURL : https://mychls-p0intapp-default-rtdb.asia-southeast1.firebasedatabase.app,
-projectId : mychls-p0intapp,
-appId : 1:229330961921:web:c2cb7f851d35828b4ee200,
+  apiKey : 'AIzaSyBbKlHBjsu9-qKpWVBBm4rFZinfJOek5Hc',
+authDomain : 'mychls-p0intapp.firebaseapp.com',
+databaseURL : 'https://mychls-p0intapp-default-rtdb.asia-southeast1.firebasedatabase.app',
+projectId : 'mychls-p0intapp',
+appId : '1:229330961921:web:c2cb7f851d35828b4ee200',
 };
 /* ▲▲▲ 書き換えここまで ▲▲▲ */
 
